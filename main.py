d16be4ba76ba4701ae02fbde83074f7a468d892c
jaredgoodin <813552@mystma.org>
2021-01-22 15:50:29 +0000

    factorial try 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def factorial (x):
+
+    result = 1
+
+    for i in range(1, x + 1):
+        result = result * 1
+        
+    return result 
+
+pint (factorial(5))
